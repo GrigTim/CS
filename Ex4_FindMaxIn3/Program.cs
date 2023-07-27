3685167fdc15b4b810fe6bfd09359184916887ce
Grig <timofeevgr19@mail.ru>
2023-07-27 13:41:17 +0300

    add ex4

---
+++ Ex4_FindMaxIn3/Program.cs
@@ -0,0 +1,18 @@
+﻿// программа принимает на вход три целых числа и выдает максимальное
+Console.Write("Введите первое число ");
+int numberA = int.Parse (Console.ReadLine () ?? "");
+Console.Write("Введите второе число ");
+int numberB = int.Parse (Console.ReadLine () ?? "");
+
+if (numberA == numberB)
+{
+    Console.WriteLine (" no max ");
+}
+if (numberA > numberB)
+{
+    Console.WriteLine ($" max = {numberA}");
+}
+else
+{
+    Console.WriteLine ($" max = {numberB}");
+}
