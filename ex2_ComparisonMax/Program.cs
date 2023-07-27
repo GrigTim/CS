778943861bcbb46fadc1d5c296510ad1fe04955f
Grig <timofeevgr19@mail.ru>
2023-07-27 12:51:36 +0300

    add homework

---
+++ ex2_ComparisonMax/Program.cs
@@ -0,0 +1,15 @@
+﻿// программа принимает на вход два целых числа и выдает, какое больше
+Console.Write("Введите первое число ");
+int numberA = int.Parse (Console.ReadLine () ?? "");
+Console.Write("Введите второе число ");
+int numberB = int.Parse (Console.ReadLine () ?? "");
+
+if (numberA > numberB)
+{
+    Console.WriteLine ($" max = {numberA}");
+}
+else
+{
+    Console.WriteLine ($" max = {numberB}");
+}
+// введение условия с elseif (для варианта когда вводимые числа равны) создает почему то ошибку. В приложении картинка
