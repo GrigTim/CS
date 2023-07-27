89aa4104ad92aa46b2f3e06e9bf96b0e4e37ae07
Grig <timofeevgr19@mail.ru>
2023-07-27 17:49:39 +0300

    add Even

---
+++ Ex6_Even/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine ("Введите целое число");
+int number = int.Parse (Console.ReadLine() ?? "");
+
+if ((number % 2) == 0)
+{
+    Console.WriteLine("yes");
+}
+else
+{
+    Console.WriteLine("no");
+}
