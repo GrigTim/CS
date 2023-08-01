2125adf8bac02d30820c654b5424532b4330d7a2
Grig <timofeevgr19@mail.ru>
2023-08-01 15:46:50 +0300

    Задача про поиск выходного

---
+++ Ex15/Program.cs
@@ -0,0 +1,17 @@
+﻿// На входную цифру отвечает, выходной день недели или нет. 
+Console.Clear();
+int day;
+while (true)
+{
+    Console.Write ("Введите номер дня: ");
+    if (int.TryParse(Console.ReadLine(), out day))
+        break;
+    Console.WriteLine ("Ошибка ввода");
+}
+if (day >7 || day <1)
+{
+    Console.WriteLine("Нет такого дня ");
+}
+if (day == 6) Console.WriteLine($" сегодня  выходной {day} - > Суббота ");
+if (day == 7) Console.WriteLine($" сегодня выходной {day} - > Воскресенье ");
+if (day < 6) Console.WriteLine($" сегодня {day} - рабочий день ");
