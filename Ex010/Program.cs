70ec3b7d7a2a893e33b6cf7a7d4d6d9caac93455
Grig <timofeevgr19@mail.ru>
2023-08-01 14:09:42 +0300

    Защита от дурака поиск второй цифры

---
+++ Ex010/Program.cs
@@ -0,0 +1,15 @@
+﻿// принимает на вход трехзначное число и на выход показывает вторую цифру этого числа
+Console.Clear();
+Console.WriteLine("Введите трехзначное число ");
+int varA;
+while (true)
+{
+    Console.Write ("Введите число: ");
+    if (int.TryParse(Console.ReadLine(), out varA))
+        break;
+    Console.WriteLine ("Ошибка ввода");
+}
+Console.WriteLine($" A = {varA}");
+int a = varA % 100;
+int b = a / 10; 
+Console.WriteLine($" {varA} -> {b}");
