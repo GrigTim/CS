ba3562d85a725060b2edbad88ae67083f5f8f532
Grig <timofeevgr19@mail.ru>
2023-08-01 16:54:54 +0300

    кратность 7 и 23

---
+++ Ex16/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine ("input number");
+int num = int.Parse (Console.ReadLine() ?? "");
+
+int a = num % 7;
+int b = num % 23;
+if (a+b ==0 )
+{
+    Console.WriteLine ("кратно ");
+}
+else 
+{
+    Console.WriteLine ("no");
+}
