2eac1098c31619ebb704d150de1b90b8cd0ca235
Grig <timofeevgr19@mail.ru>
2023-08-01 16:56:04 +0300

    показывает вторую цифру трехзначного числа

---
+++ Ex12/Program.cs
@@ -0,0 +1,15 @@
+﻿// Определить, является ли введеное второе число кратно введенному первому. Если нет, вывести остаток от деления
+Console.WriteLine ("Введите первое число ");
+int num1 = int.Parse (Console.ReadLine() ?? "");
+Console.WriteLine ("Введите второе число ");
+int num2 = int.Parse (Console.ReadLine() ?? "");
+int res = num1 % num2;
+
+if (res == 0 )
+{
+    Console.WriteLine($"{num1} кратно {num2}");
+}
+else 
+{
+    Console.WriteLine($"{num1} некратно {num2} остаток -> {res}");
+}
