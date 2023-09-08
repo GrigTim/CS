31951b48513d96650b63f2f7e8a2c777d4987c61
Grig <timofeevgr19@mail.ru>
2023-09-08 19:16:30 +0300

    упорядочить по убыванию эл-ты каждой строки двухмерного массива

---
+++ Home54/Program.cs
@@ -0,0 +1,101 @@
+﻿//Задача 54 упорядочит по убыванию элементы каждой строки двумерного массива.
+
+#region --- 00. Configuration ---
+Console.Clear();
+
+
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+var curConsoleColor = Console.ForegroundColor;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+Console.WriteLine(@"Задача 54 упорядочит по убыванию элементы каждой строки двумерного массива.");
+Console.ForegroundColor = curConsoleColor;
+Console.ForegroundColor = ConsoleColor.DarkGreen;
+#endregion
+
+#region --- 01. Input operations ---
+// Вводим число строк и столбцов
+int rows = GetNumberFromUser("Введите число строк двумерного массива ", "Ошибка ввода");
+int columns = GetNumberFromUser("Введите число столбцов двумерного массива ", "Ошибка ввода");
+
+  
+#endregion --- 01. Input operations ---
+
+#region --- 02. logic and output ---
+// Метод создающий массив (, означает что он двумерный. Трехмерный был бы [ , ,] и тд )
+// Тут у нас есть максимальный и минимальный элемент. И тип данных массива инт. 
+int[,] array = GetArray (rows, columns, 0, 10);
+PrintArray(array);
+
+
+Console.WriteLine ("   ");
+
+RepElRows(array);
+PrintArray(array);
+
+#endregion --- 02. logic and output ---
+
+
+// -------------------------- Конец программы ----------------------------------
+
+
+// -------------------------Определение методов ---------------------------------
+// Определяем функцию, выполняющую ввод целого числа с консоли
+static int GetNumberFromUser(string message, string errorMessage)
+{   
+    while(true)
+    {
+        try
+        {
+            Console.Write(message);
+            return int.Parse(Console.ReadLine() ?? "");            
+        }
+        catch (Exception exc)
+        {
+            Console.WriteLine($"{errorMessage} {exc.Message}");        
+        }
+    }
+}
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+// вот туть и происходит сортировка 
+void RepElRows(int[,] inArray)
+{    
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            int max = j;
+            for (int count = j+1; count< inArray.GetLength(1); count++)
+            {
+                if(inArray[i,count] > inArray[i,max])
+                max = count;
+            } 
+            int temp =  inArray[i, j];
+            inArray[i, j] = inArray[i, max];
+            inArray[i, max] = temp; 
+        }
+    }
+}
